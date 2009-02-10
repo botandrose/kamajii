08c9004af4698d90b8029011125a2a738fb8217a
Micah Geisel <originofstorms@gmail.com>
2009-02-10 01:23:06 -0800

    default controller to logged_exceptions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.connect 'logged_exceptions/:action/:id', :controller => 'logged_exceptions'
-  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id', :defaults => { :controller => 'logged_exceptions' }
   map.connect ':controller/:action/:id.:format'
 end
